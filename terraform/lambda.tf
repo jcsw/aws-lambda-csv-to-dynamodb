8f2818a2185d5b09914e992af9d7bc31b7cdbe76
Jonas Almeida Cezar <jcezar.al@gmail.com>
2018-11-02 10:42:45 -0300

    Refatorando terraform

---
+++ terraform/lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "extract_movies_from_s3_go" {
+  filename      = "../aws-lambda/extract_movies_from_s3/cmd/deployment.zip"
+  function_name = "extract_movies_from_s3_go"
+  role          = "${aws_iam_role.extract_movies_from_s3_role.arn}"
+  handler       = "main"
+  runtime       = "go1.x"
+  timeout       = "300"
+  reserved_concurrent_executions = 1
+}
+
+resource "aws_lambda_function" "import_movies_in_dynamodb_go" {
+  filename      = "../aws-lambda/import_movies_in_dynamodb/cmd/deployment.zip"
+  function_name = "import_movies_in_dynamodb_go"
+  role          = "${aws_iam_role.import_movies_in_dynamodb_role.arn}"
+  handler       = "main"
+  runtime       = "go1.x"
+  timeout       = "60"
+  reserved_concurrent_executions = 4
+}
